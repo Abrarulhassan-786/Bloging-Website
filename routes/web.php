17f2c2b05ca5d94199901064491e7900e98cf720
Abrarulhassan-786 <abrarulhassan336@gmail.com>
2024-07-25 15:49:46 +0500

    sidenavbar Dashboard

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::prefix('admin')->middleware(['auth','isAdmin'])->group(function(){
-    Route::get('dashboard',[DashboardController::class,'index']);
+    Route::get('dashboard',[DashboardController::class,'index'])->name('admin.dashboard');
     Route::get('/category',[CategoryController::class,'index']);
     Route::get('/add_category', [CategoryController::class, 'create'])->name('admin.add_category');
     Route::post('/add_category', [CategoryController::class,'store']);
